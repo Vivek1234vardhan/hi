948789fb1c66e6e6acbdb01ed78a9ef9ed0999db
Vivek1234vardhan <155940540+Vivek1234vardhan@users.noreply.github.com>
2024-11-12 13:42:46 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,6 @@
+
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+current_year = 2024
+year_turn_100 = current_year + (100 - age)
+print(f"Hello, {name}! You will turn 100 years old in the year {year_turn_100}.")
